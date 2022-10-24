30dc285a308c2d3196f861f9076ed4e2407dbcf4
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-10-24 18:18:20 +0000

    chore: upgrade to latest cargo version

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export { BaseSchema } from "https://deno.land/x/cargo_inspect@0.1.45/mod.ts";
+export { BaseSchema } from "https://deno.land/x/cargo_inspect@0.1.47/mod.ts";
--- src/deps_test.ts
+++ src/deps_test.ts
@@ -2,4 +2,4 @@ export * from "https://deno.land/std@0.159.0/testing/asserts.ts";
 export {
   ObjectSchema,
   StringSchema,
-} from "https://deno.land/x/cargo_inspect@0.1.45/mod.ts";
+} from "https://deno.land/x/cargo_inspect@0.1.47/mod.ts";
