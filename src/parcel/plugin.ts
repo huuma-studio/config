9d48a040b1a62912aa431dfe678c19b5421d0788
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-10-24 13:59:08 +0000

    feat: filter labels allowed in the browser

--- src/parcel/plugin.ts
+++ src/parcel/plugin.ts
@@ -17,10 +17,32 @@ export function LabelPlugin(options: {
       return {
         scripts: [
           `<script type="module">import { Label } from "/plugin-label.js";
-	Label(${JSON.stringify(options.loader())})
+	Label(${JSON.stringify(filterAllowedInBrowser(options.loader()))})
 	</script>`,
         ],
       };
     },
   };
+}
+
+/**
+ * Function to filter only explicitly allowed labels for the browser.
+ * Only labels which are appended with a "!" are used in the browser.
+ */
+function filterAllowedInBrowser(labels: Labels) {
+  const allowedLabels: Labels = {};
+  for (const key in labels) {
+    const chars = [...key];
+    if (chars[chars.length - 1] === "!") {
+      if (
+        typeof labels[key] === "string" || typeof labels[key] === "number" ||
+        typeof labels[key] === "boolean"
+      ) {
+        allowedLabels[key] = labels[key];
+        break;
+      }
+      allowedLabels[key] = filterAllowedInBrowser(<Labels> labels[key]);
+    }
+  }
+  return allowedLabels;
 }
