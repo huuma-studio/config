003ab05679d2212910a5df426694e96eeda65e7b
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-11-19 21:21:42 +0000

    text: fix tests

--- src/parcel/plugin.test.ts
+++ src/parcel/plugin.test.ts
@@ -1,29 +1,29 @@
 import { assertEquals } from "../deps_test.ts";
 import { LabelPlugin } from "./plugin.ts";
 
-const plugin = await LabelPlugin({
+const plugin = (await LabelPlugin({
   labels: () => {
     return {
       name: "Anakin",
       "name!": "Luke",
     };
   },
-});
+}));
+
+const pluginDescription = plugin.plugin();
 
 Deno.test("Cargo Label Plugin: 'name' should be 'Cargo Label Plugin'", () => {
   assertEquals(plugin.name, "Cargo Label Plugin");
 });
 
 Deno.test("Cargo Label Plugin: 'entryPoints' array should be set", () => {
-  assertEquals(plugin.entryPoints, [{
+  assertEquals(pluginDescription.entryPoints, {
     "plugin-label": new URL("main.ts", import.meta.url).href,
-  }]);
+  });
 });
 
 Deno.test("Cargo Label Plugin: plugin() call should return array with 'scripts'", () => {
-  assertEquals(plugin.plugin(), {
-    scripts: [
-      '<script type="module">import { Label } from "/plugin-label.js";\n\tLabel({"name!":"Luke"})\n\t</script>',
-    ],
-  });
+  assertEquals(pluginDescription.scripts, [
+    '<script type="module">import { Label } from "/plugin-label.js";Label({"name!":"Luke"})</script>',
+  ]);
 });
